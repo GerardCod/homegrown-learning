9f17db42e6bc2006d0d0582cd86e896d7fc6326a
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-05 13:02:34 -0500

    [ADD] Adding firebase.js file to the project

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDyybelhhZqlNeVNQBdTB-ijRHLawaXVio",
+  authDomain: "homegrown-learning.firebaseapp.com",
+  projectId: "homegrown-learning",
+  storageBucket: "homegrown-learning.appspot.com",
+  messagingSenderId: "920114469568",
+  appId: "1:920114469568:web:54c44070725441bc2eeb25",
+  measurementId: "G-C4NSWG23E7"
+};

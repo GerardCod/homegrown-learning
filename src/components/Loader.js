879af23d1a3fd8d1557f380438b8d119571053ea
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-11 16:21:48 -0500

    modify: Loader component creatd

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { FaCircleNotch } from 'react-icons/fa';
 
-
 const Loader = () => {
   return (
-    <FaCircleNotch />
+    <FaCircleNotch className="text--accent Loader" />
   );
 }
+
+export default Loader;

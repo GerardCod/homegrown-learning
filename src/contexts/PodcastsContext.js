4b0d1f4aff53eb9cf2495418ba6c7516db05ce95
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-10 20:39:26 -0500

    modify: PodcastsProvider HOC created

---
+++ src/contexts/PodcastsContext.js
@@ -0,0 +1,36 @@
+import React, { createContext, useCallback, useReducer, useRef } from 'react';
+import PodcastsReducer, {initialState} from '../reducers/PodcastReducer';
+import { database } from '../firebase';
+import { ERROR, FETCH_COLLECTION, LOADING } from '../reducers/Actions';
+import { collectIdAndData } from '../utils';
+
+export const PodcastsContext = createContext();
+
+const PodcastsProvider = ({children}) => {
+  const [state, dispatch] = useReducer(PodcastsReducer, initialState);
+  const collectionRef = useRef({});
+
+  const fetchCollection = useCallback(({onError}) => {
+    dispatch({type: LOADING});
+    collectionRef.current = database.collection('podcasts').onSnapshot(
+      snapshot => {
+        const documents = snapshot.docs.map(collectIdAndData);
+        dispatch({type: FETCH_COLLECTION, payload: documents});
+      },
+      error => {
+        dispatch({type: ERROR, payload: error.message});
+        onError(error.message);
+      }
+    );
+  }, []);
+
+  const childProps = { fetchCollection }
+
+  return (
+    <PodcastsContext.Provider value={childProps}>
+      {children}
+    </PodcastsContext.Provider>
+  );
+}
+
+export default PodcastsProvider;

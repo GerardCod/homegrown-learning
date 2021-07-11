ccb29bf9ea7451c7726f2bbb38efdf4a1791c8b8
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-11 00:44:35 -0500

    modify: finishing PodcastItem component styles finished

--- src/components/PodcastItem.js
+++ src/components/PodcastItem.js
@@ -4,9 +4,9 @@ import { FaBook } from 'react-icons/fa';
 const PodcastItem = ({podcast}) => {
   return (
     <article className="PodcastItem flex content--start">
-      <FaBook />
+      <FaBook className="text--accent PodcastItem__Icon" />
       <div className="PodcastItem__Content">
-        <h2>{podcast.title}</h2>
+        <h2 className="margin--none text--accent">{podcast.title}</h2>
         <span>Subido el 10/07/2021</span>
       </div>
     </article>

dee0e20b5dfa138b8285118344ce7bb101ba8f9e
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-10 20:14:31 -0500

    modify: PodcastsPage component created

---
+++ src/pages/Podcasts.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const PodcastsPage = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>Podcast subidos a la plataforma</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default PodcastsPage;

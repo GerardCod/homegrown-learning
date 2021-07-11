99db253831780c27bd03ab27125ce2160bc681f3
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-11 00:36:46 -0500

    modify: PodcastItem component modified

--- src/components/PodcastItem.js
+++ src/components/PodcastItem.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import { FaBook } from 'react-icons/fa';
+
+const PodcastItem = ({podcast}) => {
+  return (
+    <article className="PodcastItem flex content--start">
+      <FaBook />
+      <div className="PodcastItem__Content">
+        <h2>{podcast.title}</h2>
+        <span>Subido el 10/07/2021</span>
+      </div>
+    </article>
+  );
+}
+
+export default PodcastItem;

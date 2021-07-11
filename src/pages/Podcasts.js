5816e20e0bc6caeeab7300107264776040547aea
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-11 00:37:24 -0500

    modify: Implementing PodcastItem component in podcasts page

--- src/pages/Podcasts.js
+++ src/pages/Podcasts.js
@@ -1,17 +1,32 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
+import PodcastItem from '../components/PodcastItem';
+import { PodcastsContext } from '../contexts/PodcastsContext';
+import { onError } from '../utils';
 
 const PodcastsPage = () => {
+  const { state, fetchCollection, collectionRef } = useContext(PodcastsContext);
 
   useEffect(() => {
+    fetchCollection({ onError });
+    const subscribe = collectionRef.current;
 
-  }, []);
+    return () => {
+      subscribe();
+    };
+  }, [collectionRef, fetchCollection]);
 
   return (
     <Fragment>
       <main className="Page flex content--center">
         <div className="container">
           <h1>Podcast subidos a la plataforma</h1>
-          
+          {
+            state.loading ? 
+            <p>Cargando...</p> :
+            (state.podcasts && state.podcasts.length > 0) ?
+            state.podcasts.map((element) => <PodcastItem podcast={element} key={`podcast: ${element.id}`} />) :
+            <p>No hay podcasts en la plataforma.</p>
+          }
         </div>
       </main>
     </Fragment>

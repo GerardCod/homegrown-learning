13dc639ea4664ae3277f708c1d4cf080dffaf31e
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-12 15:58:17 -0500

    modify: Adding signIn function in AuthContext

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,46 @@
+import React, { createContext, useCallback } from 'react';
+import AuthReducer, { initialState } from '../reducers/AuthReducer';
+import { ERROR, LOADING, USER_LOGGED } from '../reducers/Actions';
+import { auth, database } from '../firebase';
+import { collectIdAndData } from '../utils';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  const signIn = useCallback(async ({email, password, role: {name}}, {onError}) => {
+    dispatch({type: LOADING});
+    try {
+      const user = await searchUser(email, name);
+      if (user) {
+        await auth.signInWithEmailAndPassword(email, password);
+      }
+      localStorage.setItem('user', JSON.stringify(user));
+      dispatch({type: USER_LOGGED, payload: user});
+    } catch (error) {
+      dispatch({type: ERROR, payload: error.message});
+      onError(error.message);
+    }
+  }, []);
+
+  const searchUser = useCallback(async (email, roleName) => {
+    try {
+      const userCollection = await database.collection('accounts').where('email', '==', email).where('role.name', '==', roleName).get();
+      const user = collectIdAndData(userCollection.docs[0]);
+      return user;
+    } catch (error) {
+      return error;
+    }
+  }, []);
+
+  const childProps = { state, signIn };
+
+  return (
+    <AuthContext.Provider value={ childProps }>
+      { children }
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;

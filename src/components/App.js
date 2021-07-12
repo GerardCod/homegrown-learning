f49ba8afec251f90bbbfd2b6063933cb62b2174b
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-12 16:08:02 -0500

    modify: Adding AuthProvider provider added to App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,19 @@ import Login from "../pages/Login";
 import ForgotPassword from "../pages/ForgotPassword";
 import PlatformRouter from "./PlatformRouter";
 
+import AuthProvider from '../contexts/AuthContext';
+
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/forgotpassword" component={ForgotPassword} />
-        <Route path="/platform" component={PlatformRouter} />
-      </Switch>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/forgotpassword" component={ForgotPassword} />
+          <Route path="/platform" component={PlatformRouter} />
+        </Switch>
+      </AuthProvider>
     </BrowserRouter>
   );
 }

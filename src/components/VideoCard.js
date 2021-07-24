b7e62a3c55c101a7bf66ede01faebc41f340fd7c
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-24 14:18:57 -0500

    fix: Fixing routing between VideosPage and VideoDetailsPage component

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -3,12 +3,12 @@ import { FaPlay } from 'react-icons/fa';
 import { Link, useRouteMatch } from 'react-router-dom';
 
 const VideoCard = ({ video }) => {
-  const match = useRouteMatch('/platform/videos');
+  const { path } = useRouteMatch('/platform/videos');
 
   return (
     <Fragment>
       <article className="VideoCard">
-        <Link to={`${match}/${video.id}`} className="width--full height--full">
+        <Link to={`${path}/${video.id}`} className="width--full height--full">
           <div className="grid VideoCard__Content width--full height--full">
             <figure className="VideoCard__Illustration flex items--center content--center">
               <FaPlay />

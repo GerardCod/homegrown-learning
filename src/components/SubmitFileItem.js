32dfea4c6ed7b99e265360f6f78a875164759cfd
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-10 12:48:39 -0500

    merge: Add submit evidence feature finished

--- src/components/SubmitFileItem.js
+++ src/components/SubmitFileItem.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react';
-import { FaFileAlt } from 'react-icons/fa';
+import { FaFileAlt, FaTimes } from 'react-icons/fa';
 
-const SubmitFileItem = ({ file }) => {
-  
+const SubmitFileItem = ({ file, removeFile }) => {
   return (
     <Fragment>
-      <article className="PodcastItem flex content--start">
+      <article className="PodcastItem flex content--start items--center">
       <FaFileAlt className="text--info PodcastItem__Icon" />
-      <div className="PodcastItem__Content">
+      <div className="PodcastItem__Content width--full flex content--between">
         <h2 className="margin--none PodcastItem__Title text--info">
-          {file.name}
+          <a href={file.url} target="_blank" rel="noreferrer"> { file.name } </a>
         </h2>
+        <FaTimes className="cursor--pointer" onClick={() => removeFile(file)}/>
       </div>
     </article>
     </Fragment>

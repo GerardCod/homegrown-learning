f85e81fc312bc5216cf74c512976ca2677444030
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-09-12 13:47:22 -0500

    modify: Fixing routing between activity details and material details

--- src/components/MaterialLinkItem.js
+++ src/components/MaterialLinkItem.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { FaFile } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const MaterialLinkItem = function Component({material}) {
   return (
@@ -7,9 +8,11 @@ const MaterialLinkItem = function Component({material}) {
       <article className="PodcastItem flex content--start">
         <FaFile className="text--primary PodcastItem__Icon" />
         <div className="PodcastItem__Content">
-          <h2 className="margin--none PodcastItem__Title">
-            <a href={material.link} target="_top" rel="opener">{material.title}</a>
-          </h2>
+          <Link to={{
+            pathname: material.link
+          }} replace>
+            <h2 className="margin--none PodcastItem__Title">{material.title}</h2>
+          </Link>
           <span className="PodcastItem__Date">Subido el {material.postDate} a las {material.postTime}</span>
         </div>
       </article>
